b7166bb308e95729806964858a48e4ff2329b60e
dantemallozzi05 <dante.mallozzi@hotmail.com>
2026-01-27 15:26:09 -0500

    Created src/utils/seed.py to have common reproducible seed for every script

--- src/transformer.py
+++ src/transformer.py
@@ -6,4 +6,22 @@ import torch
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 
-from torch_geometric
+
+def main():
+
+    graph_dir = "data/processed/graph"
+    x_loc = os.path.join(graph_dir, "x.npy")
+    y_loc = os.path.join(graph_dir, "y.npy")
+    e_loc = os.path.join(graph_dir, "i_edge.npy")
+    
+    X = np.load(x_loc).astype(np.float32)
+    y = np.load(y_loc).astype(np.int64)
+    i_edge = np.load(e_loc).astype(np.int64)
+
+    X_t = torch.tensor(X, dtype=torch.float32)
+    y_t = torch.tensor(y, dtype=torch.long)
+    i_edge_t = torch.tensor(edge_index, dtype=torch.long)
+
+    data = Data(
+
+    )
